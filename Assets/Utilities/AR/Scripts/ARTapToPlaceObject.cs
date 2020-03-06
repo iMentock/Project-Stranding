8a6289d2f775a003dc76c61bbcaffb3f9e1e8fff
Virgil Martinez <virgilmartinez@gmail.com>
2020-03-05 19:26:20 -0600

    New Features
    Can now go into AR Placement, detect planes (horizontal & vertical), place cube object and move it around said plane(s). Tomorrow will start working with geo coordinates & Mapbox.

---
+++ Assets/Utilities/AR/Scripts/ARTapToPlaceObject.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject gameObjectToInstantiate;
+
+    private GameObject spawnedObject;
+    private ARRaycastManager _arRaycastManager;
+    private Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private void Awake() {
+        // Required above so safe to use
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition) {
+        if(Input.touchCount > 0) {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    void Update()
+    {
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        // Takes touch position and static list 'hits' and detects if inside plane that AR detected
+        if(_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon)) {
+            var hitPose = hits[0].pose;
+
+            // If there is an object already then move it etc. (could limit number and all that here)
+            if(spawnedObject == null) {
+                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
+            } else {
+                spawnedObject.transform.position = hitPose.position;
+            }
+
+        }
+    }
+}
