ffd3052f3845991fd683ed13e5c4471ec7b00e57
Virgil Martinez <virgilmartinez@gmail.com>
2020-03-07 09:21:02 -0600

    Added features
    Added text on top of text cube which is where the geolocation will go.

---
+++ Assets/Prefabs/AR/Testing Cube/TextManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public Text geoLocationText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        geoLocationText.text = "I have changed the text!";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
